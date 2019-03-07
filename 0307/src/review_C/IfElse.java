c42940e5f5f564a97e9b3421677ef95ca9cdc83b
jeong <lmk6989@naver.com>
2019-03-07 10:38:27 +0900

    C와 유사한 Java 문법 맛보기

---
+++ 0307/src/review_C/IfElse.java
@@ -0,0 +1,14 @@
+package review_C;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int x = 5;
+		
+		if(x>=5) {
+			System.out.println("x는 5 이상입니다.");
+		}
+		else {
+			System.out.println("x는 5 미만입니다.");
+		}
+	}
+}
